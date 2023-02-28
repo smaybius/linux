4e1b2cbc2ea6cf548da42b400d92369af36487cf
Danila Tikhonov <danila@jiaxyga.com>
2023-02-28 19:17:41 +0300

    dt-bindings: clock: camcc-sm7150: fix license

--- include/dt-bindings/clock/qcom,sm7150-camcc.h
+++ include/dt-bindings/clock/qcom,sm7150-camcc.h
@@ -1,14 +1,7 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
 /*
  * Copyright (c) 2018, The Linux Foundation. All rights reserved.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 and
- * only version 2 as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Copyright (c) 2023, Danila Tikhonov <danila@jiaxyga.com>
  */
 
 #ifndef _DT_BINDINGS_CLK_QCOM_CAM_CC_SM7150_H
